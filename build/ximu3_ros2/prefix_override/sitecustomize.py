01c3c2dca13149ec94d42acf4636caf54aa48fbe
buck@Tstation <well.robotics@gmail.com>
2023-11-14 12:11:50 -0600

    remove

--- build/ximu3_ros2/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/bucky/Documents/repos/HardwareIndividual/x-IMU3-ros2/install/ximu3_ros2'
--- build/ximu3_ros2/share/ximu3_ros2/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/bucky/Documents/repos/HardwareIndividual/x-IMU3-ros2/build/ximu3_ros2"
